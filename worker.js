48069e0a394759f3bb89e80972241d5c71107194
bowo-prasetyo <bpraz@yahoo.com>
2026-05-28 21:22:31 +0900

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,191 +1,176 @@
-function complex(re, im) {
-  return { re, im };
-}
-
-function cis(theta) {
-  return {
-    re: Math.cos(theta),
-    im: Math.sin(theta)
-  };
-}
-
-function add(a, b) {
-  return {
-    re: a.re + b.re,
-    im: a.im + b.im
-  };
-}
-
-function multiply(a, b) {
-  return {
-    re: a.re * b.re - a.im * b.im,
-    im: a.re * b.im + a.im * b.re
-  };
-}
-
-function probability(a) {
-  return a.re * a.re + a.im * a.im;
-}
-
 const SQRT2 = Math.sqrt(2);
 
-const singleQubitGates = {
+function probability(re, im) {
+  return re * re + im * im;
+}
+
+function applySingleQubitGate(
+  re,
+  im,
+  matrix,
+  targetQubit,
+  qubitCount
+) {
+
+  const size = 1 << qubitCount;
+  const bit = qubitCount - 1 - targetQubit;
+
+  const outRe = new Float64Array(size);
+  const outIm = new Float64Array(size);
+
+  for (let i = 0; i < size; i++) {
+
+    if (((i >> bit) & 1) === 0) {
+
+      const j = i | (1 << bit);
+
+      const ar = re[i];
+      const ai = im[i];
+
+      const br = re[j];
+      const bi = im[j];
+
+      const m00 = matrix[0][0];
+      const m01 = matrix[0][1];
+      const m10 = matrix[1][0];
+      const m11 = matrix[1][1];
+
+      outRe[i] =
+        m00.re * ar - m00.im * ai +
+        m01.re * br - m01.im * bi;
+
+      outIm[i] =
+        m00.re * ai + m00.im * ar +
+        m01.re * bi + m01.im * br;
+
+      outRe[j] =
+        m10.re * ar - m10.im * ai +
+        m11.re * br - m11.im * bi;
+
+      outIm[j] =
+        m10.re * ai + m10.im * ar +
+        m11.re * bi + m11.im * br;
+    }
+  }
+
+  return {
+    re: outRe,
+    im: outIm
+  };
+}
+
+function applyCNOT(
+  re,
+  im,
+  control,
+  target,
+  qubitCount
+) {
+
+  const size = 1 << qubitCount;
+
+  const outRe = new Float64Array(re);
+  const outIm = new Float64Array(im);
+
+  const controlBit = qubitCount - 1 - control;
+  const targetBit = qubitCount - 1 - target;
+
+  for (let i = 0; i < size; i++) {
+
+    if (((i >> controlBit) & 1) === 1) {
+
+      const j = i ^ (1 << targetBit);
+
+      if (i < j) {
+
+        const tr = outRe[i];
+        const ti = outIm[i];
+
+        outRe[i] = outRe[j];
+        outIm[i] = outIm[j];
+
+        outRe[j] = tr;
+        outIm[j] = ti;
+      }
+    }
+  }
+
+  return {
+    re: outRe,
+    im: outIm
+  };
+}
+
+const gates = {
 
   I: [
-    [complex(1, 0), complex(0, 0)],
-    [complex(0, 0), complex(1, 0)]
+    [{ re: 1, im: 0 }, { re: 0, im: 0 }],
+    [{ re: 0, im: 0 }, { re: 1, im: 0 }]
   ],
 
   X: [
-    [complex(0, 0), complex(1, 0)],
-    [complex(1, 0), complex(0, 0)]
+    [{ re: 0, im: 0 }, { re: 1, im: 0 }],
+    [{ re: 1, im: 0 }, { re: 0, im: 0 }]
   ],
 
   Y: [
-    [complex(0, 0), complex(0, -1)],
-    [complex(0, 1), complex(0, 0)]
+    [{ re: 0, im: 0 }, { re: 0, im: -1 }],
+    [{ re: 0, im: 1 }, { re: 0, im: 0 }]
   ],
 
   Z: [
-    [complex(1, 0), complex(0, 0)],
-    [complex(0, 0), complex(-1, 0)]
+    [{ re: 1, im: 0 }, { re: 0, im: 0 }],
+    [{ re: 0, im: 0 }, { re: -1, im: 0 }]
   ],
 
   H: [
-    [complex(1 / SQRT2, 0), complex(1 / SQRT2, 0)],
-    [complex(1 / SQRT2, 0), complex(-1 / SQRT2, 0)]
-  ],
-
-  S: [
-    [complex(1, 0), complex(0, 0)],
-    [complex(0, 0), complex(0, 1)]
-  ],
-
-  T: [
-    [complex(1, 0), complex(0, 0)],
-    [complex(0, 0), cis(Math.PI / 4)]
+    [{ re: 1 / SQRT2, im: 0 }, { re: 1 / SQRT2, im: 0 }],
+    [{ re: 1 / SQRT2, im: 0 }, { re: -1 / SQRT2, im: 0 }]
   ]
 };
-
-function cloneState(state) {
-  return state.map(v => complex(v.re, v.im));
-}
-
-function applySingleQubitGate(state, gate, targetQubit) {
-
-  const m = singleQubitGates[gate];
-  const result = cloneState(state);
-
-  for (let i = 0; i < 4; i++) {
-
-    if (((i >> (1 - targetQubit)) & 1) === 0) {
-      
-      const j = i | (1 << (1 - targetQubit));
-
-      const a = state[i];
-      const b = state[j];
-
-      result[i] = add(
-        multiply(m[0][0], a),
-        multiply(m[0][1], b)
-      );
-
-      result[j] = add(
-        multiply(m[1][0], a),
-        multiply(m[1][1], b)
-      );
-    }
-  }
-
-  return result;
-}
-
-function applyCNOT(state) {
-
-  const result = cloneState(state);
-
-  result[0] = state[0];
-  result[1] = state[1];
-  result[2] = state[3];
-  result[3] = state[2];
-
-  return result;
-}
-
-function measure(state) {
-
-  const probs = state.map(probability);
-
-  const rnd = Math.random();
-
-  let accum = 0;
-  let measured = 0;
-
-  for (let i = 0; i < probs.length; i++) {
-    accum += probs[i];
-
-    if (rnd < accum) {
-      measured = i;
-      break;
-    }
-  }
-
-  const collapsed = [
-    complex(0, 0),
-    complex(0, 0),
-    complex(0, 0),
-    complex(0, 0)
-  ];
-
-  collapsed[measured] = complex(1, 0);
-
-  return {
-    measured,
-    state: collapsed
-  };
-}
 
 self.onmessage = (e) => {
 
   const {
     type,
     gate,
-    state,
-    targetQubit
+    targetQubit,
+    control,
+    target,
+    re,
+    im,
+    qubitCount
   } = e.data;
 
   if (type === 'gate') {
 
     const result = applySingleQubitGate(
-      state,
-      gate,
-      targetQubit
+      re,
+      im,
+      gates[gate],
+      targetQubit,
+      qubitCount
     );
 
-    self.postMessage({
-      type: 'state',
-      state: result
-    });
+    self.postMessage(result, [
+      result.re.buffer,
+      result.im.buffer
+    ]);
   }
 
   if (type === 'cnot') {
 
-    const result = applyCNOT(state);
+    const result = applyCNOT(
+      re,
+      im,
+      control,
+      target,
+      qubitCount
+    );
 
-    self.postMessage({
-      type: 'state',
-      state: result
-    });
-  }
-
-  if (type === 'measure') {
-
-    const result = measure(state);
-
-    self.postMessage({
-      type: 'measurement',
-      measured: result.measured,
-      state: result.state
-    });
+    self.postMessage(result, [
+      result.re.buffer,
+      result.im.buffer
+    ]);
   }
 };
